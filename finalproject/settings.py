371238b5c947de2171910a5f0d5b882ece708bc7
ahmedshimmas <mushayyy007@gmail.com>
2025-05-16 22:36:38 +0500

    moved info files to the notes folder

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -76,10 +76,22 @@ WSGI_APPLICATION = 'finalproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#postgresql database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'final_project',
+        'USER': 'postgres',
+        'PASSWORD': 'Mushu5763',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
